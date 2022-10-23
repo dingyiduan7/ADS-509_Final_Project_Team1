26d1a933ee25a005a3133d44366d675d7b7e827b
Eva Chow <77718240+evchow@users.noreply.github.com>
2022-10-23 11:03:07 -0700

    Delete api_keys.py

--- api_keys.py
+++
@@ -1,7 +0,0 @@
-
-# From the "project set-up" experience
-api_key = 'gr8FLAP27CWHwSVs4u2mfk4Ww'
-api_key_secret = 'wlqQpvKTl7CJjri6cfcc6WNF9IdPIlFh0VAFGs0nCbwl4cc29q'
-bearer_token = 'AAAAAAAAAAAAAAAAAAAAAFnLgwEAAAAApS8Ed%2FvGySUeAbpW3EQJiSC2nMQ%3DTmsQTaYi2wcPsaWGL4yBWj4eMTqjUw1ahiU8ESlL1eG8tZ3GKF'
-
-
